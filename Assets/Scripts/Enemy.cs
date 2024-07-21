0f19eb7e2d6313362fe7ff9b8e7e2670d3948058
Hamdani <hamdaniryuuken@gmail.com>
2024-07-21 21:13:13 +0700

    Add enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,49 @@ namespace NoodleEater
 {
     public class Enemy : MonoBehaviour
     {
+        private bool _movingRight = true;
+
+        private EnemyConfig _enemyConfig;
+
+        private void Start()
+        {
+            _enemyConfig = EnemyConfig.Load();
+        }
+
+        private void Update()
+        {
+            MoveAliens();
+        }
+
+        private void MoveAliens()
+        {
+            Vector3 movement = Vector3.right * (_enemyConfig.Speed * Time.deltaTime);
+            if (!_movingRight)
+            {
+                movement = -movement;
+            }
+
+            transform.Translate(movement);
+            
+            if (_movingRight && transform.position.x > _enemyConfig.RightBoundary)
+            {
+                MoveDown();
+                _movingRight = false;
+            }
+
+            if (!_movingRight && transform.position.x < _enemyConfig.LeftBoundary)
+            {
+                MoveDown();
+                _movingRight = true;
+            }
+        }
+
+        private void MoveDown()
+        {
+            Vector3 moveDown = new Vector3(0, -_enemyConfig.MoveDownAmount, 0);
+            transform.position += moveDown;
+        }
+        
         private void OnTriggerEnter2D(Collider2D other)
         {
             Bullet bullet = other.GetComponent<Bullet>();
